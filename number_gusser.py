7a0135c50d5aea5b9b71e2ef36dd16a868761f09
Shlokjain03 <148685250+Shlokjain03@users.noreply.github.com>
2024-06-23 22:32:03 +0530

    Update number_gusser.py

--- number_gusser.py
+++ number_gusser.py
@@ -1,35 +1,56 @@
 import random
+import tkinter as tk
+from tkinter import simpledialog, messagebox
 
-top_of_range = input("Type a number: ")
-
-if top_of_range.isdigit():
-    top_of_range = int(top_of_range)
-
-    if top_of_range <= 0:
-        print('Please type a number larger than 0 next time.')
-        quit()
-else:
-    print('Please type a number next time.')
-    quit()
-
-random_number = random.randint(0, top_of_range)
-guesses = 0
-
-while True:
-    guesses += 1
-    user_guess = input("Make a guess: ")
-    if user_guess.isdigit():
-        user_guess = int(user_guess)
+def start_game():
+    top_of_range = simpledialog.askstring("Input", "Type a number:")
+    if top_of_range and top_of_range.isdigit():
+        top_of_range = int(top_of_range)
+        if top_of_range <= 0:
+            messagebox.showwarning("Invalid input", "Please type a number larger than 0 next time.")
+            return
     else:
-        print('Please type a number next time.')
-        continue
+        messagebox.showwarning("Invalid input", "Please type a number next time.")
+        return
 
-    if user_guess == random_number:
-        print("You got it!")
-        break
-    elif user_guess > random_number:
-        print("You were above the number!")
-    else:
-        print("You were below the number!")
+    random_number = random.randint(0, top_of_range)
+    guesses = 0
 
-print("You got it in", guesses, "guesses")
+    def make_guess():
+        nonlocal guesses
+        nonlocal random_number
+        user_guess = entry.get()
+        if user_guess.isdigit():
+            user_guess = int(user_guess)
+        else:
+            messagebox.showwarning("Invalid input", "Please type a number next time.")
+            return
+
+        guesses += 1
+        if user_guess == random_number:
+            messagebox.showinfo("Result", f"You got it in {guesses} guesses!")
+            root.quit()
+        elif user_guess > random_number:
+            result_label.config(text="You were above the number!")
+        else:
+            result_label.config(text="You were below the number!")
+
+    root = tk.Tk()
+    root.title("Number Guessing Game")
+
+    prompt_label = tk.Label(root, text="Make a guess:")
+    prompt_label.pack()
+
+    entry = tk.Entry(root)
+    entry.pack()
+
+    guess_button = tk.Button(root, text="Guess", command=make_guess)
+    guess_button.pack()
+
+    result_label = tk.Label(root, text="")
+    result_label.pack()
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    start_game()
